d6fef127b9ac5a609e6deffc63034d49b8b9240e
eduard herman <herman_eduard2000@yahoo.com>
2015-09-28 18:01:18 +0200

    v32

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module ImageCrop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
+    
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
