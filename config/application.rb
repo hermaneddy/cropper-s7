5368ba62313bae49312632783a682ba501adc237
eduard herman <herman_eduard2000@yahoo.com>
2015-09-28 18:06:24 +0200

    v22

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,21 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module ImageCrop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+       # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # force application to not access the DB
+    # or load models when precompiling your assets
+    config.assets.initialize_on_precompile = false
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
