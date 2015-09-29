61483003a155bcd5f5925ed50d30c07bd96e2186
eduard herman <herman_eduard2000@yahoo.com>
2015-09-29 09:47:06 +0200

    test
    test

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150527101545) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
-    t.string   "link"
+    t.text   "link"
   end
 
   create_table "messages", force: true do |t|
